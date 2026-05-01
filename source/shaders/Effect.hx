7ed936bdf5c176649f0c98e0981463f3f3391573
SuperHero2010 <Imjoohn2@gmail.com>
2026-05-01 09:37:02 +0700

    tTry importing the correct FlxShader

--- source/shaders/Effect.hx
+++ source/shaders/Effect.hx
@@ -6,14 +6,14 @@ import flixel.system.FlxAssets.FlxShader;
  * Base effect class for shader effects (Psych / JS Engine compatible Lua API).
  */
 interface CameraStackShader {
-	public var shader:FlxShader;
+	public var shader:flixel.system.FlxAssets.FlxShader;
 }
 
 class Effect {
 	public function new() {
 	}
 
-	public function setValue(shader:FlxShader, variable:String, value:Float) {
+	public function setValue(shader:flixel.system.FlxAssets.FlxShader, variable:String, value:Float) {
 		Reflect.setProperty(Reflect.getProperty(shader, variable), 'value', [value]);
 	}
 }
