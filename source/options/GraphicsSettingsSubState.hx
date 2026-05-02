6222618de2ea60f0ce853b24f3eaa7ce1d4eae5b
SuperHero2010 <Imjoohn2@gmail.com>
2026-05-02 10:45:19 +0700

    Add noteShaders

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -39,6 +39,12 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 			BOOL);
 		addOption(option);
 
+		var option:Option = new Option('Note Shaders',
+			"If unchecked, disables note shaders.\nPlease use the noteSkin older than psych v0.6.x!",
+			'noteShaders',
+			BOOL);
+		addOption(option);
+
 		var option:Option = new Option('GPU Caching', //Name
 			"If checked, allows the GPU to be used for caching textures, decreasing RAM usage.\nDon't turn this on if you have a shitty Graphics Card.", //Description
 			'cacheOnGPU',
