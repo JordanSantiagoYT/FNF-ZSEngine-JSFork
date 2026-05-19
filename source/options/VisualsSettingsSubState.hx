f0c64c3db139fa9d314230f823f001a608eb7bc4
SuperHero2010 <Imjoohn2@gmail.com>
2026-05-19 09:32:38 +0700

    Small update

--- source/options/VisualsSettingsSubState.hx
+++ source/options/VisualsSettingsSubState.hx
@@ -126,7 +126,7 @@ class VisualsSettingsSubState extends BaseOptionsMenu
 			'Change default of the note hit amount (0.02 is default)',
 			'hitHealth',
 			FLOAT);
-		option.scrollSpeed = 0.2;
+		option.scrollSpeed = 0.1;
 		option.minValue = 0.0001;
 		option.maxValue = 1;
 		option.changeValue = 0.0001;
@@ -137,7 +137,7 @@ class VisualsSettingsSubState extends BaseOptionsMenu
 			'Change default of the note miss amount (0.1 is default)',
 			'missHealth',
 			FLOAT);
-		option.scrollSpeed = 0.2;
+		option.scrollSpeed = 0.1;
 		option.minValue = 0.0001;
 		option.maxValue = 1;
 		option.changeValue = 0.0001;
@@ -158,7 +158,7 @@ class VisualsSettingsSubState extends BaseOptionsMenu
 			'How much health will drain per note when health drain is enabled.',
 			'drain',
 			FLOAT);
-		option.scrollSpeed = 0.2;
+		option.scrollSpeed = 0.1;
 		option.minValue = 0.0001;
 		option.maxValue = 1;
 		option.changeValue = 0.0001;
@@ -169,7 +169,7 @@ class VisualsSettingsSubState extends BaseOptionsMenu
 			'Set the limit to prevent death (0.023 is default)',
 			'maxDrain',
 			FLOAT);
-		option.scrollSpeed = 0.2;
+		option.scrollSpeed = 0.1;
 		option.minValue = 0.023;
 		option.maxValue = 0.5;
 		option.changeValue = 0.0001;
